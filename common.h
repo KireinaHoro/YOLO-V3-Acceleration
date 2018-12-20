fe13c0de820a44e3b2ca6c76335925fa1e0626ef
KireinaHoro <i@jsteward.moe>
2018-12-20 08:39:53 +0000

    clean code

--- common.h
+++ common.h
@@ -6,28 +6,27 @@
 #define CUDA_MEM_ALIGN 256
 
 // alignptr
-int8_t * alignPtr(int8_t * ptr, uintptr_t to);
+int8_t *alignPtr(int8_t *ptr, uintptr_t to);
 
-int8_t * nextWorkspacePtr(int8_t * ptr, uintptr_t previousWorkspaceSize);
+int8_t *nextWorkspacePtr(int8_t *ptr, uintptr_t previousWorkspaceSize);
 
-void setUniformOffsets(const int num_segments, const int offset, int * d_offsets, cudaStream_t stream);
+void setUniformOffsets(const int num_segments, const int offset, int *d_offsets,
+                       cudaStream_t stream);
 
 /**
  * Determine the usage of temporary memory for cub sort
  * The cub::DeviceSegmentedRadixSort can be used for batched (segmented) sort.
  */
 template <typename KeyT, typename ValueT>
-size_t cubSortPairsWorkspaceSize(int num_items, int num_segments)
-{
-    size_t temp_storage_bytes = 0;
-    cub::DeviceSegmentedRadixSort::SortPairsDescending(
-	(void *)NULL, temp_storage_bytes,
-	(const KeyT *)NULL, (KeyT *)NULL,
-	(const ValueT *)NULL, (ValueT *)NULL,
-	num_items,     // # items
-	num_segments,  // # segments
-	(const int *)NULL, (const int *)NULL);
-    return temp_storage_bytes;
+size_t cubSortPairsWorkspaceSize(int num_items, int num_segments) {
+  size_t temp_storage_bytes = 0;
+  cub::DeviceSegmentedRadixSort::SortPairsDescending(
+      (void *)NULL, temp_storage_bytes, (const KeyT *)NULL, (KeyT *)NULL,
+      (const ValueT *)NULL, (ValueT *)NULL,
+      num_items,    // # items
+      num_segments, // # segments
+      (const int *)NULL, (const int *)NULL);
+  return temp_storage_bytes;
 }
 
 #endif
